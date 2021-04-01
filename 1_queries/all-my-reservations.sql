c81f39827f94240eec3c0569649f42a149e31cea
KateIsabelle <kate.davis887@gmail.com>
2021-04-01 19:09:49 +0000

    add query to database for user reservations info

--- 1_queries/all-my-reservations.sql
+++ 1_queries/all-my-reservations.sql
@@ -1,10 +1,9 @@
-SELECT properties.*, reservations.*, AVG(rating) as average_rating
-FROM properties
-JOIN reservations ON properties.id = reservations.property_id
-JOIN property_reviews ON properties.id = property_reviews.property_id
+SELECT properties.*, reservations.*, avg(rating) as average_rating
+FROM reservations
+JOIN properties ON reservations.property_id = properties.id
+JOIN property_reviews ON properties.id = property_reviews.property_id 
 WHERE reservations.guest_id = 1
-AND end_date < now()::date
+AND reservations.end_date < now()::date
 GROUP BY properties.id, reservations.id
-ORDER BY start_date
+ORDER BY reservations.start_date
 LIMIT 10;
-
