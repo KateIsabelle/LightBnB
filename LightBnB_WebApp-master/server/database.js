4d8adc6e3b53c71cffe05e2f24dd0f301a72f262
KateIsabelle <kate.davis887@gmail.com>
2021-04-01 21:44:06 +0000

    write addProperty query to database.js

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -31,6 +31,7 @@ const getUserWithEmail = function(email) {
 }
 exports.getUserWithEmail = getUserWithEmail;
 
+//
 const getUserWithId = function(id) {
   return pool.query(`
   SELECT * FROM users
@@ -42,6 +43,7 @@ const getUserWithId = function(id) {
 }
 exports.getUserWithId = getUserWithId;
 
+//
 const addUser =  function(userObj) {
   return pool.query(`
   INSERT INTO users (name, email, password)
@@ -57,11 +59,6 @@ exports.addUser = addUser;
 
 /// Reservations
 
-/**
- * Get all reservations for a single user.
- * @param {string} guest_id The id of the user.
- * @return {Promise<[{}]>} A promise to the reservations.
- */
 const getAllReservations = function(guest_id) {
   return pool.query(`
   SELECT properties.*, reservations.*, avg(rating) as average_rating
@@ -83,85 +80,67 @@ exports.getAllReservations = getAllReservations;
 
 /// Properties
 
-/**
- * Get all properties.
- * @param {{}} options An object containing query options.
- * @param {*} limit The number of results to return.
- * @return {Promise<[{}]>}  A promise to the properties.
- */
  const getAllProperties = function(options, limit = 10) {
-   //1
+  
   const queryParams = [];
-  //2
+  
   let queryString = `
   SELECT properties.*, avg(property_reviews.rating) as average_rating
   FROM properties
   JOIN property_reviews ON properties.id = property_id
   WHERE true
   `;
-  //3
+  
   if (options.city) {
     queryParams.push(`%${options.city}%`);
     queryString += `AND city LIKE $${queryParams.length} `;
   }
-  //
+  
   if (options.owner_id) {
     queryParams.push(options.owner_id);
     queryString += `AND owner_id = $${queryParams.length} `;
   }
-  //
+  
   if (options.minimum_price_per_night) {
     queryParams.push(Number(options.minimum_price_per_night)*100);
     queryString += `AND cost_per_night >= $${queryParams.length} `;
   }
-  //
+  
   if (options.maximum_price_per_night) {
     queryParams.push(Number(options.maximum_price_per_night)*100);
     queryString += `AND cost_per_night <= $${queryParams.length} `;
   }
-  //
+  
   if (options.minimum_rating) {
     queryParams.push(Number(options.minimum_rating));
     queryString += `AND rating >= $${queryParams.length} `;
   }
-  //
+  
   queryParams.push(limit);
   queryString += `
   GROUP BY properties.id
   ORDER BY cost_per_night
   LIMIT $${queryParams.length};
   `;
-  // 
+  
   console.log(queryString, queryParams);
-  // 
+  
   return pool.query(queryString, queryParams)
   .then(res => res.rows);
 }
 exports.getAllProperties = getAllProperties;
 
-
-/**
- * Add a property to the database
- * @param {{}} property An object containing all of the property details.
- * @return {Promise<{}>} A promise to the property.
- */
+// 
 const addProperty = function(property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+  return pool.query(`
+  INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, street, city, province, post_code, country, parking_spaces, number_of_bathrooms, number_of_bedrooms)
+  VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
+  RETURNING *;
+  `, [property.owner_id, property.title, property.description, property.thumbnail_photo_url, property.cover_photo_url, property.cost_per_night, property.street, property.city, property.province, property.post_code, property.country, property.parking_spaces, property.number_of_bathrooms, property.number_of_bedrooms])
+  .then(res => {
+    console.log(res.rows[0]);
+    return res.rows[0]
+  }).catch(e => console.log(e))
 }
 exports.addProperty = addProperty;
 
-
-// SELECT properties.*, avg(property_reviews.rating) as average_rating
-//   FROM properties
-//   JOIN property_reviews ON properties.id = property_id
-//   WHERE true
-//   AND city LIKE '%ancouve%'
-//   AND cost_per_night >= 500 
-//   AND cost_per_night <= 1000 AND rating >= $4 
-//   GROUP BY properties.id
-//   ORDER BY cost_per_night
-//   LIMIT $5;
-//    [ '%ancouve%', 500, 1000, 3, 20 ]
